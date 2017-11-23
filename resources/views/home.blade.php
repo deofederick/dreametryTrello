21b63e030cb933c9e68354ffbea028a4412c1d92
Vysvaldis <vysvaldis@github.com>
2017-11-23 13:51:45 +0800

    fixed board scroll

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,30 @@
 @extends('layouts.app')
+
+@section('content')
+
+<div class="container" id="paddingtop">
+  <div class="row">
+    <div class="col-md-4 placeholder justify-content-center text-center" id="tasktoday">
+      <img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+      <div class="centered text-white"><h1>0</h1></div>
+      <h4>Today</h4>
+      <div class="text-muted">mm/dd/yyyy</div>
+    </div>
+
+    <div class="col-md-4 placeholder justify-content-center text-center" id="tasktoday">
+      <img src="" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+      <div class="centered text-white"><h1>0</h1></div>
+      <h4>This week</h4>
+      <div class="text-muted">mm/dd/yyyy</div>
+    </div>
+
+    <div class="col-md-4 placeholder justify-content-center text-center" id="tasktoday">
+      <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
+      <div class="centered text-white"><h1>0</h1></div>
+      <h4>This month</h4>
+      <div class="text-muted">mm/dd/yyyy</div>
+    </div>
+  </div>
+</div>
+
+@endsection
--- resources/views/pages/boardreg.blade.php
+++ resources/views/pages/boardreg.blade.php
@@ -5,13 +5,13 @@
 <div class="container" id="paddingtop">
     <div class="row">
         <div class="col-md-6">
-            <div class="card">
+            <div class="card" id="overflows">
               <div class="card-header bg-success text-white">SELECT BOARD</div>
               <!-- <div class="card-body bg-success text-white text-center">
                 <h3 class="panel-hero-title justify-content-center"><i class="fa fa-clipboard"></i> 999999</h3>
                 <p class="card-text">boards</p>
               </div> -->
-              <ul class="list-group list-group-flush">
+              <ul class="list-group list-group-flush" >
                 <li class="list-group-item">
                     <p class="card-text">Lorem ipsum dolor sit amet</br>
                     <small class="text-muted">November 16, 2017</small>
@@ -30,7 +30,7 @@
         </div>
 
         <div class="col-md-6">
-            <div class="card">
+            <div class="card" id="overflows">
               <div class="card-header bg-warning text-white">REGISTERED BOARDS</div>
               <!-- <div class="card-body bg-warning text-white text-center">
                 <h3 class="panel-hero-title justify-content-center"><i class="fa fa-clipboard"></i> 999999</h3>
