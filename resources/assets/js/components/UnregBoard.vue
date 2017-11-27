aa12626843e59753abf1b91afca0562a13caa950
Vysvaldis <vysvaldis@github.com>
2017-11-27 13:48:04 +0800

    this..get fixed

--- resources/assets/js/components/UnregBoard.vue
+++ resources/assets/js/components/UnregBoard.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-        <ul class="list-group list-group-flush" v-for="unregboard in unregboards" :key="unregboard.boardId">
-            <li class="list-group-item">
-                <p class="card-text">{{ unregboard.boardName }}</p>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"  v-for="unregboard in unregboards" :key="unregboard.boardId">
+                <div class="card-block">
+                    <div class="card-title">
+                        {{ unregboard.boardName }}
+                    </div>
+                </div>
             </li>
         </ul>
     </div>
@@ -12,25 +16,9 @@
 export default {
     data(){
         return{
-            unregboards:[
-                {
-                    "boardName":"Dan Manalili - L2",
-                    "boardId":"5935fc808823fdbe2875c07a",
-                    "organization":"Dreametry Inc."
-                },
-                {
-                    "boardName":"Dillon - L1",
-                    "boardId":"59190bf0b27a6f308820ef7d",
-                    "organization":"Dreametry Inc."
-                },
-                {
-                    "boardName":"Dreametry - Santosh Varughese",
-                    "boardId":"58a5006e9985e088628f308b",
-                    "organization":"Dreametry Inc."
-                }
-            ]
+            unregboards:[]
         }
-    }/* , created: function(){
+    } , created: function(){
         this.fetchUnreg()
     }, methods:{
         fetchUnreg: function () {
@@ -38,7 +26,7 @@ export default {
                 this.unregboards = resp.unRegBoards;
             }).bind(this);
         }
-    } */
+    } 
 }
 </script>
 
